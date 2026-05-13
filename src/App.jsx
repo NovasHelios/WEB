d773cfd1cdfd8d9a899527a152b21ae0ab57a829
dlaguswls333 <sk01048745480@gmail.com>
2026-05-13 23:23:19 +0900

    회원가입

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-
+import Background from "./pages/Background"
+// import SignIn from "./pages/SignIn/normal"
+import BusinessSignIn2 from "./pages/SignIn/business2.jsx"
 
 function App() {
   return (
     <>
-      
+      <Background>
+        <BusinessSignIn2 />
+      </Background>
     </>
   )
 }
