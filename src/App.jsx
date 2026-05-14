0c6dc15bdb6a06d645bcc45b6dee526470ffcbe5
jjhhkkang <jjhhkkang@gmail.com>
2026-05-14 17:13:32 +0900

    회원가입 중간완성본

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
+import { useState } from "react"
+import { Route, BrowserRouter, Routes } from "react-router-dom"
 import Background from "./pages/Background"
-import SignIn from "./pages/SignIn/normal"
+import BusinessSignIn1 from "./pages/SignIn/business1"
 import BusinessSignIn2 from "./pages/SignIn/business2.jsx"
 
+
+
 function App() {
+
+  const [page, setPage] = useState("business1")
   return (
-    <>
+    <BrowserRouter>
       <Background>
-        <BusinessSignIn2 />
+        <Routes>
+          <Route path="/" element={<BusinessSignIn1 />} />
+          <Route path="/business2" element={<BusinessSignIn2 />} />
+        </Routes>
       </Background>
-    </>
-  )
+    </BrowserRouter>
+  );
 }
 
 export default App
