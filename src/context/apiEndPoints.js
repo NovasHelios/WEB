3683e63537bd79e7cdcdebc492419a0f707c0955
jjhhkkang <jjhhkkang@gmail.com>
2026-05-28 17:57:10 +0900

    signin 수정

--- src/context/apiEndPoints.js
+++
@@ -1,6 +0,0 @@
-export const Api = {
-  SignUp: "http://10.80.163.133:8080/api/auth/signup",
-  Login: "http://10.80.163.133:8080/api/auth/login",
-  EmailVarify: "http://10.80.163.133:8080/api/auth/email/verify",
-  EmailSend: "http://10.80.163.133:8080/api/auth/email/send",
-};
